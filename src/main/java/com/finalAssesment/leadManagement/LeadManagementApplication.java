4b8b9939b3a7030b18cf97a99dc414c333d75aae
thalha-dev <dev.thalha@gmail.com>
2024-03-27 15:52:25 +0530

    initial files & setup db

---
+++ src/main/java/com/finalAssesment/leadManagement/LeadManagementApplication.java
@@ -0,0 +1,13 @@
+package com.finalAssesment.leadManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeadManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeadManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finalAssesment/leadManagement/LeadManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalAssesment.leadManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeadManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
